ce6d224d87025ca7093b10d7bf0f38372f3618e4
WalidZa <walid.zakan@gmail.com>
2022-12-27 21:54:30 +0100

    gg

--- js/categories.js
+++ js/categories.js
@@ -41,3 +41,39 @@ hideButton.addEventListener("click", function () {
   // Remove the "visible" class from the sidebar
   sidebar.classList.remove("visible");
 });
+// ! ========================== SideBar Toggling ===========================
+
+// Initialize the sliders
+const sliders = document.querySelectorAll(".product-slider");
+
+for (let i = 0; i < sliders.length; i++) {
+  createSlider(sliders[i]);
+
+  function createSlider(slider) {
+    // Select the arrow buttons and product items for this slider
+    const leftArrow = slider.querySelectorAll(".arrow.left");
+    const rightArrow = slider.querySelectorAll(".arrow.right");
+    const items = slider.querySelectorAll(".product-slider .slide");
+
+    // Initialize variables to track the current item and item width
+    let currentItem = 0;
+    const itemWidth = items[0].offsetWidth;
+
+    // Add event listeners to the arrow buttons
+    leftArrow.addEventListener("click", function () {
+      currentItem = Math.max(currentItem - 1, 0);
+      slider.scrollTo({
+        left: currentItem * itemWidth,
+        behavior: "smooth",
+      });
+    });
+
+    rightArrow.addEventListener("click", function () {
+      currentItem = Math.min(currentItem + 1, items.length - 1);
+      slider.scrollTo({
+        left: currentItem * itemWidth,
+        behavior: "smooth",
+      });
+    });
+  }
+}
