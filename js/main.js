dd92659c825bce9390509275038909ecb8d7250e
WalidZa <walid.zakan@gmail.com>
2022-12-30 16:30:31 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@ var toggleButtonNav = document.querySelector("header nav .toggle-menu");
 var navBar = document.querySelector("header nav ul ");
 
 toggleButtonNav.addEventListener("click", function () {
-    navBar.classList.toggle("visible")
+  navBar.classList.toggle("visible");
 });
+
+var btns = document.querySelectorAll("#myBtn");
+
+// Get the modal
+var modal = document.getElementById("form-page");
+
+// When the user clicks on the button, open the modal
+for (var i = 0; i < btns.length; i++) {
+  // Get the current button
+  var btn = btns[i];
+
+  // Add a click event listener to the button
+  btn.addEventListener("click", function () {
+    // Toggle the display of the form
+    modal.style.display = "block";
+  });
+}
+var cls = document.getElementsByClassName("close")[0];
+
+cls.onclick = function () {
+  modal.style.display = "none";
+};
