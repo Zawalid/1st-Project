f0024212a08b3bfca8c9920e16094d2deb49c843
WalidZa <walid.zakan@gmail.com>
2022-12-31 00:52:53 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -6,24 +6,46 @@ toggleButtonNav.addEventListener("click", function () {
   navBar.classList.toggle("visible");
 });
 // ! ========================== Form Toggling ===========================
-
-var btns = document.querySelectorAll("#myBtn");
-
-// Get the modal
-var modal = document.getElementById("form-page");
-// When the user clicks on the button, open the modal
-for (var i = 0; i < btns.length; i++) {
-  // Get the current button
-  var btn = btns[i];
-
-  // Add a click event listener to the button
+// ? ==== Sign In ====
+var signinBtns = document.querySelectorAll("#signinBtn");
+var signin = document.getElementById("form-page-signin");
+for (var i = 0; i < signinBtns.length; i++) {
+  var btn = signinBtns[i];
   btn.addEventListener("click", function () {
-    // Toggle the display of the form
-    modal.classList.add("open")
+    signin.classList.add("open");
   });
 }
-var cls = document.getElementsByClassName("close")[0];
-
+var cls = document.getElementById("clsignin");
 cls.onclick = function () {
-    modal.classList.remove("open")
+  signin.classList.remove("open");
 };
+// ? ==== Sign UP ====
+var signupBtns = document.querySelectorAll("#signupBtn");
+var signup = document.getElementById("form-page-signup");
+for (var i = 0; i < signupBtns.length; i++) {
+  var btn = signupBtns[i];
+  btn.addEventListener("click", function () {
+    signup.classList.add("open");
+  });
+}
+var cls = document.getElementById("clsignup");
+cls.onclick = function () {
+  signup.classList.remove("open");
+};
+var swapsin = document.getElementById("swapsin")
+swapsin.onclick = function () {
+  signin.classList.remove("open")
+  signup.classList.add("open")
+}
+var swapsup = document.getElementById("swapsup")
+swapsup.onclick = function () {
+  signup.classList.remove("open")
+  signin.classList.add("open")
+}
+
+
+
+
+
+
+
